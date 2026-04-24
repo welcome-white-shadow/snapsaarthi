92dd9f194009395fc858bdf73bbd57c12a72ecf9
mark9933 <mayurmali9933@gmail.com>
2026-04-24 15:20:29 +0530

    Fixed: Reverted to server mode for dynamic routes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   }
 };
 
